074ec50b87a7b0f656e3aef67e28275acb8a3669
SantiagoAV <81932949+SantiAV25@users.noreply.github.com>
2023-02-20 00:40:33 -0500

    AddDeInfozoneProyects

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+//#region Atributos
 let ZonaDeCartas = document.getElementById('CartasZoneProyects');
 let CartasColumnasInicio = document.getElementsByClassName('CartasColumnas');
 let InfoZoneProyects = document.getElementById('InfoZoneProyects');
-let ArrayDirreciones = ["deadLiftIcone.png","PerfumeIcone.png"];
 let ArrayDatos = [{Titulo: 'Gym', Descripcion: 'An App That I made for de register of a gym', link:'https://github.com/Pablo736/Gym', direccionFoto:'deadLiftIcone.png'},
-                {Titulo: 'Perfume', Descripcion: 'An App That I made for an Perfumery', link:'https://github.com/Pablo736/Gym', direccionFoto:'PerfumeIcone.png.png'}];
+                {Titulo: 'Perfume', Descripcion: 'An App That I made for an Perfumery', link:'https://github.com/Pablo736/Gym', direccionFoto:'PerfumeIcone.png'}];
 let WaitingIconeDirreccion = "waitingIcone.png";
-
+//#endregion
 function deleteCartasColumnas(){
         while(CartasColumnasInicio.length > 0)
         {
@@ -19,7 +19,7 @@ function deleteCartasColumnas(){
         CreateCartasColumnasClass(2);
 
         let CartasColumnas = document.getElementsByClassName("CartasColumnas");
-        MixArray(ArrayDirreciones);
+        MixArray(ArrayDatos);
         CreacionDeLogos(CartasColumnas,2);
 
 
@@ -46,10 +46,10 @@ function CreacionDeLogos(CartasColumnas, cantidad){
 
     for (let index2 = 0; index2 < cantidad; index2++) {
         for (let index = 0; index < 3; index++) {
-            if(ArrayDirreciones.length > index && !validacion){
+            if(ArrayDatos.length > index && !validacion){
                 var nuevaClaseCartas = document.createElement("div");
                 nuevaClaseCartas.setAttribute("class","Cartas");
-                nuevaClaseCartas.innerHTML = `<img src="Iconos/${ArrayDirreciones[index]}">`;
+                nuevaClaseCartas.innerHTML = `<img src="Iconos/${ArrayDatos[index].direccionFoto}" onclick="Informacion('${ArrayDatos[index].Titulo}','${ArrayDatos[index].Descripcion}','${ArrayDatos[index].link}')">`;
                 CartasColumnas[index2].insertAdjacentElement("beforeend",nuevaClaseCartas);                
             }else{
                 validacion = true;
@@ -79,6 +79,7 @@ function MixArray(Array){
 
 //#endregion
 
+//#region Zona De InfomracionDeProyectos
 function Informacion(Titulo,Descripcion,Link)
 {
     InfoZoneProyects.innerHTML = "";
@@ -87,8 +88,15 @@ function Informacion(Titulo,Descripcion,Link)
     ElementoInformacion.setAttribute("id","Informacion");
     ElementoInformacion.innerHTML = `<h3>${Titulo}</h3>
                                     <p>${Descripcion}</p>
-                                    <a>${Link}</a>
-                                    <button>Volver</button>`;
+                                    <a href="${Link}">${Link}</a>
+                                    <button class="btn btn-outline-success btn-lg" onclick="botonVolverInfo()">Volver</button>`;
     InfoZoneProyects.insertAdjacentElement("beforeend",ElementoInformacion);
 
 }
+
+function botonVolverInfo(){
+    InfoZoneProyects.innerHTML = `<button type="button" class="btn btn-outline-success btn-lg" onclick="deleteCartasColumnas()">Mix it</button>
+    <button type="button" class="btn btn-outline-success btn-lg">pick One</button>`;
+}
+
+//#endregion
